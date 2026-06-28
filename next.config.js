f26dc715b1bc16028e7bba9d4f7ff3f17008ecdc
John James <82113632+rhomanjames@users.noreply.github.com>
2026-06-28 06:18:01 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  staticPageGenerationTimeout: 180,
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 module.exports = nextConfig;
